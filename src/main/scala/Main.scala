0453f62b9f73d7c3cf57d68804fbe78b652baedb
Juan ARISTIZABAL MUNOZ <juanarismu@gmail.com>
2022-08-30 17:04:07 +0200

    test

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,7 @@
 @main def hello: Unit =
   println("Hello Paris!")
   println(msg)
+  println(msg2)
 
 def msg = "I was compiled by Scala 3. ;D This is how our tutorial starts!"
+def msg2 = "New changes from Branch A testing"
